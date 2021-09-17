a6ceeb75fdde6899a36c9781910664769ef8d1e5
Kevin Martinsson <kevinmartinsson1995@gmail.com>
2021-09-17 16:44:03 +0200

    Gjort om och förbättrat min kod

--- script.js
+++ script.js
@@ -1,67 +1,91 @@
-const namn= document.getElementById("namn");  
-const lösen= document.getElementById("lösen"); 
-const loginButton = document.getElementById("login-form-submit"); 
+const session = "username";
+ // Försöker att logga in/tries to log in 
+function logIn() {
 
- 
- 
-loginButton.addEventListener("click", function() {  
-    const name= document.getElementById("namn");  
-const pass= document.getElementById("lösen"); 
+    // Hämtar användarnamn och lösenord /get username and password input objects
+    const pass = document.getElementById("lösen"); 
+    const name = document.getElementById("namn");
 
-    
-    
+    // sätter värdet ifrån inputs till namn och lösen/set values from inputs to namn and lösen
     const namn = name.value;
     const lösen = pass.value;
-    if (namn === "test" && lösen === "1234") {  
+
+    // om rätt "refernser" /if credentials are correct
+    if (namn === "test" && lösen === "1234") {
         
-        const varning = document.getElementById("varning") 
-        varning.innerHTML = ""    
-        
-        localStorage.setItem("loginForm", true); 
-        const loginView = document.getElementById("logIn") 
-        const pageView = document.getElementById("page1") 
-        loginView.style.display = "none"
-        pageView.style.display = "block"
-          
-    
-        
+        // Sätter 'test' med värdet 'namn' till local storage/set 'test' with value 'namn' to local storage
+        localStorage.setItem(session, namn)
+
+        // visa page sidan/display page view
+        displayView('pageView')
+    } else {
+
+        // annars om fel referenser angivits, visa varnings sidan /otherwise user provided wrong credentials, display varning view
+        displayView('varningView') 
+
     }
-     else {
-     const varning = document.getElementById("varning") 
-     varning.innerHTML = "Fel anvädnarnamn eller lösenord" 
-     varning.style.color = "red"
-     }
 
-});
+};
 
-(function() { 
+// tar bort 'test' från locale storage och visa login sidan/removes 'test' from local storage and displays login view
+function logOut (){
+    localStorage.removeItem(session) 
+    displayView('loginView')
+};
 
+// visa login sidan eftersom användaren angivit fel referenser/login information/displays login view, because user made mistake with his credentials
+function tillbaka (){
+    displayView('loginView')
+};
 
-    const loginView = document.getElementById("logIn") 
+// visa specifik sida/shows specified view
+function displayView(view) {
+    console.log(view);
+    // hämta alla sidor/get all views
     const pageView = document.getElementById("page1") 
-   
-    const isValidated = localStorage.getItem("loginForm") 
-    console.log(isValidated)
-    if (isValidated){ 
-           
-        loginView.style.display = "none"
-        pageView.style.display = "block"  
-        console.log(true) 
-    }
-   else { 
+    const loginView = document.getElementById("logIn") 
+    const varningView = document.getElementById("varning") 
      
-    loginView.style.display = "block"
-    pageView.style.display = "none"  
-    console.log(false) 
-   }
-   
-})(); 
-function logOut (){  
-    const loginView = document.getElementById("logIn") 
-    const pageView = document.getElementById("page1") 
-    localStorage.removeItem("loginForm") 
-    loginView.style.display = "block"
-    pageView.style.display = "none"  
-    console.log("logOut") 
+    
+     // dölj alla sidor/hide all views
+    pageView.style.display = "none"
+    loginView.style.display = "none"
+    varningView.style.display = "none" 
+     
+
+    // visa den specifika sidan/display specified view
+    if (view === 'pageView') {
+        pageView.style.display = "block"
+    }
+    
+    if (view === 'loginView') {
+        loginView.style.display = "block"
+    }
+
+    if (view === 'varningView') {
+        varningView.style.display = "block"
+    } 
+    
 }
 
+// IIFEe som bestämmer vilken sida som ska visas/immediately-invoked function expresion which determines which view should be displayed
+(function() { 
+
+    // När sidan blir refreshad, dölj alla sidor/when the page is refreshed, hide all views
+    displayView('none')
+
+    // hämta 'username' värdet ifrån locale storage/get 'username' value from local storage
+    const isValidated = localStorage.getItem(session) 
+
+    // om 'username' värdet finns/if 'username' value exist
+    if (isValidated){ 
+
+        // visa page sidan/display page view
+        displayView('pageView')
+    } else {
+
+        // annars om användaren inte är inloggad visa login sidan/otherwise user is not logged in, so display login view
+        displayView('loginView')
+    }
+
+})();
